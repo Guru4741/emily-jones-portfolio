d727d83db8dd6e4a90d5f934975c9e62e9974912
Gaurav Choudhary <gaurav4741@gmail.com>
2020-12-06 00:16:46 +0530

    Implemented Grid Layout

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var app = express();
 var path = require('path');
 
 // viewed at http://localhost:8080
+app.use(express.static('./'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(9090);
