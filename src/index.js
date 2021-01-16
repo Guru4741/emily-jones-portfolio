ec36e822847964443c829071e7ed52cdcc5612d0
Apple <apple@Apples-MacBook-Air.local>
2021-01-16 19:23:08 +0530

    feat: Added About Section
    Added About Me Counts and Skills Section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import "bootstrap";
+import "jquery";
+import 'waypoints/lib/noframework.waypoints';
+import counterUp from 'counterup2';
+
 import { run } from './app/app';
 import "./sass/main.scss";
 
@@ -7,5 +12,30 @@ import { ComponentService } from './app/component.service';
 const alertService = new AlertService();
 const componentService = new ComponentService();
 
+//run(alertService, componentService);
 
-run(alertService, componentService);
+const el = [...document.querySelectorAll('[data-toggle="counter-up"]')];
+
+el.forEach(e => {
+    new Waypoint( {
+        element: e,
+        handler: function() { 
+            counterUp( e ) 
+            this.destroy()
+        },
+        offset: 'bottom-in-view',
+    } )
+    
+})
+
+
+// Skills Section
+// const skillsEl = document.querySelector('.skills__content');
+
+// $('.skills__content').waypoint(function() {
+//     $('.progress .progress-bar').each(function() {
+//       $(this).css("width", $(this).attr("aria-valuenow") + '%');
+//     });
+//   }, {
+//     offset: '80%'
+//   });
